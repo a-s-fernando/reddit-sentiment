c8feb142eeb3710f3b88ac939d6b36c57aba5a1e
TobyRJ23 <toby.r.jackson23@gmail.com>
2023-05-19 10:59:49 +0100

    add ecs service

--- terraform/main.tf
+++ terraform/main.tf
@@ -455,3 +455,31 @@ resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attach
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+
+resource "aws_ecs_service" "main" {
+ name                               = "${var.name}-service-${var.environment}"
+ cluster                            = aws_ecs_cluster.main.id
+ task_definition                    = aws_ecs_task_definition.main.arn
+ desired_count                      = 2
+ deployment_minimum_healthy_percent = 50
+ deployment_maximum_percent         = 200
+ launch_type                        = "FARGATE"
+ scheduling_strategy                = "REPLICA"
+ 
+ network_configuration {
+   security_groups  = var.ecs_service_security_groups
+   subnets          = var.subnets.*.id
+   assign_public_ip = false
+ }
+ 
+ load_balancer {
+   target_group_arn = var.aws_alb_target_group_arn
+   container_name   = "${var.name}-container-${var.environment}"
+   container_port   = var.container_port
+ }
+ 
+ lifecycle {
+   ignore_changes = [task_definition, desired_count]
+ }
+}
