292d96b0818873d5d59f71b7a9ebb9ffc154345c
TobyRJ23 <toby.r.jackson23@gmail.com>
2023-05-22 17:02:15 +0100

    increase cpu and ram

--- terraform/main.tf
+++ terraform/main.tf
@@ -400,8 +400,8 @@ resource "aws_ecs_task_definition" "sentiment-task" {
   family = "sentiment-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = 256
-  memory                   = 512
+  cpu                      = 8192
+  memory                   = 16384
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   task_role_arn            = aws_iam_role.ecs_task_role.arn
   container_definitions = <<DEFINITION
