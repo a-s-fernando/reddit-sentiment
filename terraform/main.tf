72a15f769bbda3fdc0b08f29a784ecd4be829112
TobyRJ23 <toby.r.jackson23@gmail.com>
2023-05-23 16:48:48 +0100

    change terraform directory name

--- terraform/main.tf
+++ terraform/main.tf
@@ -525,7 +525,7 @@ resource "aws_ecs_service" "sentiment-ecs-service" {
  name                               = "sentiment-ecs-service"
  cluster                            = aws_ecs_cluster.sentiment-cluster.id
  task_definition                    = aws_ecs_task_definition.sentiment-task.arn
- desired_count                      = 1
+ desired_count                      = 2
  deployment_minimum_healthy_percent = 50
  deployment_maximum_percent         = 200
  launch_type                        = "FARGATE"
