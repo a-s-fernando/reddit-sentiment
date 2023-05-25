cef68d9de9c49969e9dea37002265f8bee50aadb
Alain Sidath Fernando <fernandoalsth@gmail.com>
2023-05-25 17:56:45 +0100

    typehints

--- Dashboard/pages/visualisation.py
+++ Dashboard/pages/visualisation.py
@@ -13,9 +13,9 @@ import numpy as np
 from numpy import ndarray
 from wordcloud import WordCloud
 from pandas import DataFrame
-
+BANNED_WORDS_FILEPATH = "./swearWords.txt"
 # Load in a list of banned words to exclude from the graph.
-with open("./swearWords.txt") as swears:
+with open(BANNED_WORDS_FILEPATH) as swears:
     ban_words = []
     for word in swears.readlines():
         ban_words.append(word.strip())
@@ -76,7 +76,7 @@ def filter_text(filtered, opposite, keyword) -> str:
     return text[:-1]
 
 
-def custom_colours(*args, **kwargs):
+def custom_colours(*args, **kwargs) -> str:
     """Returns a colour scheme matching the Social Sleuth branding"""
     colour_scheme = ["#00A6FB", "#0582CA", "#006494", "#003554", "#051923"]
     return choice(colour_scheme)
@@ -230,7 +230,7 @@ layout = html.Div(
     Input('radioitems', 'value'),
     State('input', 'value')
 )
-def update_wordclouds(n_clicks: int, swears: int, keyword: str):
+def update_wordclouds(n_clicks: int, swears: int, keyword: str) -> tuple:
     """Return two wordclouds in png format based on inputted values for the swear filter and keyword."""
     if keyword:
         positive_img, negative_img = BytesIO(), BytesIO()
